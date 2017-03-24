fc8219ace000ac613da7cb15edb7791c81234a56
Carlos del Prado <carlos.delprado@11paths.com>
2017-03-24 10:24:12 +0100

    employee:boss

---
+++ lib/plugins/bot-plugin-employee/laura.intent.employee.boss.d.ts
@@ -0,0 +1,3 @@
+import { BotBuilder } from '@telefonica/bot-core';
+declare var _default: ((session: BotBuilder.Session, args: any, next: Function) => void)[];
+export default _default;
--- src/plugins/bot-plugin-employee/index.ts
+++ src/plugins/bot-plugin-employee/index.ts
@@ -2,10 +2,12 @@ import { BotBuilder } from '@telefonica/bot-core';
 
 import info from './laura.intent.employee.info';
 import group from './laura.intent.employee.group';
+import boss from './laura.intent.employee.boss';
 
 const plugin = new BotBuilder.Library('employee');
 
 plugin.dialog('laura.intent.employee.info', info);
 plugin.dialog('laura.intent.employee.group', group);
+plugin.dialog('laura.intent.employee.boss', boss);
 
 export default plugin;
